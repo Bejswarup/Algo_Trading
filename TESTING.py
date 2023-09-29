7515e2c41271519646f259e9c344a94ff4e3d7d0
Swarup Bej <111442608+Bejswarup@users.noreply.github.com>
2023-09-29 21:53:11 +0530

    Update TESTING.py

--- TESTING.py
+++ TESTING.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
 from Intraday_Option_trading import *
-enctocken = "K5HwKEAiavdCr1q0s4IUzRUgbIlw9mZ5mRs/LxlgMnkTTyhhwQTJLd6dEZmIvT5/BZpR7sdqaWrI1jKRhW0qCAm+YBO7DyE2XBiBxBdjxWnUPygu9938Xw=="
+enctocken = "your_enctoken"
 
 option = App(enctocken)
-# option.taking_trade()
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1900)  # Adjust the width as needed
-
-# pd.set_option('display.max_rows', None)
-print(pd.DataFrame(option.kite.margins()))
+option.taking_trade()
